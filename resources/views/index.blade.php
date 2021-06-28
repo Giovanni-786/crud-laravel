f1514ace5eba7cef6c621c9bf279ff060b3ceea2
giovanni-786 <giovanni.lima786@gmail.com>
2021-06-28 15:47:04 -0300

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+@extends('templates.template')
+
+@section('content')
+    <h1 class="text-center">Crud!</h1>
+
+     <div class="col-8 m-auto">
+     <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Título</th>
+      <th scope="col">Autor</th>
+      <th scope="col">Preço</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table> 
+    </div>
+@endsection
